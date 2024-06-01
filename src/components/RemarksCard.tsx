9805544210480e2b129b7c8b69fb318eb169d8df
Pugialli <joaopugialli@gmail.com>
2024-06-01 19:03:40 -0300

    responsive

--- src/components/RemarksCard.tsx
+++ src/components/RemarksCard.tsx
@@ -16,16 +16,16 @@ export function RemarksCard({
     <div className="col-span-2 lg:col-span-4">
       {divided ? (
         <Card className="flex h-full w-full flex-col items-center gap-1 bg-background/70 p-2 text-center lg:flex-row lg:gap-4 lg:p-4">
-          <CardTitle className="lg:w-120 text-xxs font-extrabold lg:text-2xl">
+          <CardTitle className="text-xxs font-extrabold lg:w-120 lg:text-2xl">
             {title}
           </CardTitle>
-          <CardContent className="text-xxss p-0 font-medium lg:text-xl">
+          <CardContent className="p-0 text-xxss font-medium lg:text-xl">
             {children}
           </CardContent>
         </Card>
       ) : (
         <Card className="flex h-full w-full items-center justify-center bg-background/70 p-4 text-center">
-          <CardContent className="text-xxss w-full p-0 font-medium lg:text-xl">
+          <CardContent className="w-full p-0 text-xxss font-medium lg:text-xl">
             {children}
           </CardContent>
         </Card>
